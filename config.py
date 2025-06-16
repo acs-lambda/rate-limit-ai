8002f5f48c21f4b807cf3bcf5f53c005666ab5ae
Anay Pant <24apant@athenian.org>
2025-06-16 05:04:01 -0700

    Add AUTH_BP environment variable check in config.py for improved error handling

--- config.py
+++ config.py
@@ -9,6 +9,13 @@ logger = logging.getLogger()
 # AWS Region
 AWS_REGION = os.environ.get("AWS_REGION")
 
+# Auth Bypass
+AUTH_BP = os.environ.get("AUTH_BP")
+
 if not AWS_REGION:
     logger.error("AWS_REGION environment variable not set.")
-    raise ValueError("AWS_REGION is a required environment variable.") 
+    raise ValueError("AWS_REGION is a required environment variable.")
+
+if not AUTH_BP:
+    logger.error("AUTH_BP environment variable not set.")
+    raise ValueError("AUTH_BP is a required environment variable.") 
